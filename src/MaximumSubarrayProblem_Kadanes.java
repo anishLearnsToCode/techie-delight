31d01609d91214b81d6458f32740ffdbaad0a930
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2019-07-25 22:04:54 +0200

    updates kadanes to return multiple ranges

--- src/MaximumSubarrayProblem_Kadanes.java
+++ src/MaximumSubarrayProblem_Kadanes.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 // https://www.techiedelight.com/maximum-subarray-problem-kadanes-algorithm/
 public class MaximumSubarrayProblem_Kadanes {
     public static void main(String[] args) {
@@ -5,19 +8,29 @@ public class MaximumSubarrayProblem_Kadanes {
         System.out.println(maximumSubArraySum(array));
     }
 
-    private static int maximumSubArraySum(int[] array) {
+    private static List<Range> maximumSubArraySum(int[] array) {
         int[] subArraySums = new int[array.length];
         int result = 0, currentSum = 0;
+        List<Range> subArrayIndices = new ArrayList<>();
 
-        for(int element : array) {
-            currentSum += element;
+        for(int index = 0, startIndex = 0 ; index < array.length ; index++) {
+            currentSum += array[index];
             if (currentSum <= 0) {
                 currentSum = 0;
+                startIndex = index + 1;
+            }
+
+            if (currentSum > result) {
+                subArrayIndices = new ArrayList<>();
+                subArrayIndices.add(new Range(startIndex, index));
+                result = currentSum;
+            } else if (currentSum == result) {
+                subArrayIndices.add(new Range(startIndex, index));
             }
 
             result = Math.max(result, currentSum);
         }
 
-        return result;
+        return subArrayIndices;
     }
 }
