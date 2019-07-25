9f3044ab2d8b9fc8dfaa4935cd6a55d52e1d6192
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2019-07-25 23:44:33 +0200

    solves sub array having sum - k

---
+++ src/SubArrayWithSumExists.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SubArrayWithSumExists {
+    public static void main(String[] args) {
+        int[] array = StdIn.takenputAndGetArray();
+        int sum = StdIn.getIntInput();
+        System.out.println(subArraySumExists(array, sum));
+    }
+
+    private static boolean subArraySumExists(int[] array, int requiredSum) {
+        return !subArraySumRanges(array, requiredSum).isEmpty();
+    }
+
+    private static List<Range> subArraySumRanges(int[] array, int requiredSum) {
+        List<Range> ranges = new ArrayList<>();
+        Map<Integer, List<Integer>> sumIndices = new HashMap<>();
+        sumIndices.put(0, new ArrayList<>());
+        sumIndices.get(0).add(-1);
+
+        for (int index = 0, startIndex = 0, sum = 0 ; index < array.length ; index++) {
+            sum += array[index];
+
+            if (sumIndices.containsKey(sum - requiredSum)) {
+                for (int sumIndex : sumIndices.get(sum - requiredSum)) {
+                    ranges.add(new Range(sumIndex + 1, index));
+                }
+            }
+
+            if (!sumIndices.containsKey(sum)) {
+                sumIndices.put(sum, new ArrayList<>());
+            }
+            sumIndices.get(sum).add(index);
+        }
+
+        return ranges;
+    }
+}
---
+++ src/SubSetSumInArrayExists.java
@@ -0,0 +1,35 @@
+// https://www.techiedelight.com/find-subarray-having-given-sum-given-array/
+public class SubSetSumInArrayExists {
+    public static void main(String[] args) {
+        int[] array = StdIn.takenputAndGetArray();
+        int sum = StdIn.getIntInput();
+        System.out.println(subArraySumExists(array, sum));
+    }
+
+    private static boolean subArraySumExists(int[] array, int sum) {
+        int rows = array.length, columns = sum + 1;
+        boolean[][] subArraySumExists = new boolean[rows][columns];
+
+        // first column -- all true: can be formed by taking empty sub set
+        for (int row = 0, column = 0 ; row < rows ; row++) {
+            subArraySumExists[row][column] = true;
+        }
+
+        // first row - depends on the first element in the array
+        for (int row = 0, column = 1 ; column < columns ; column++) {
+            subArraySumExists[row][column] = array[row] == column ;
+        }
+
+        // rest of the grid
+        for (int row = 1 ; row < rows ; row++) {
+            for (int column = 1 ; column < columns ; column++) {
+                subArraySumExists[row][column] =
+                        subArraySumExists[row - 1][column]
+                        || array[row] == column
+                        || (array[row] < column && subArraySumExists[row - 1][column - array[row]]) ;
+            }
+        }
+
+        return subArraySumExists[rows - 1][columns - 1];
+    }
+}
