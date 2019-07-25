682d9290f58ed13501ce68df4663a97b752e2243
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2019-07-26 00:43:06 +0200

    solves largest possible number from set

---
+++ src/LargestNumberPossibleFromSetOfGivenNumbers.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+// https://www.techiedelight.com/find-largest-number-possible-set-given-numbers/
+public class LargestNumberPossibleFromSetOfGivenNumbers {
+    public static void main(String[] args) {
+        int[] array = StdIn.takenputAndGetArray();
+        System.out.println(getLargestNumber(array));
+    }
+
+    private static String getLargestNumber(int[] array) {
+        return Arrays
+                .stream(array)
+                .mapToObj(element -> element + "")
+                .sorted(new NumberComparator())
+                .collect(Collectors.joining());
+    }
+
+    private static class NumberComparator implements Comparator<String> {
+
+        @Override
+        public int compare(final String first, final String second) {
+            if (first.isEmpty() && second.isEmpty()) {
+                return 0;
+            } else if (first.isEmpty()) {
+                return -1;
+            } else if (second.isEmpty()) {
+                return 1;
+            }
+
+            if (first.charAt(0) == second.charAt(0)) {
+                return compare(first.substring(1), second.substring(1));
+            }
+
+            return first.charAt(0) > second.charAt(0) ? -1 : 1 ;
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            return super.equals(other);
+        }
+    }
+}
