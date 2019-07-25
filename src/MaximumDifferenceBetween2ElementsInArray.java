98e419920cb2a51605702a09b3e2384b4cadab08
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2019-07-25 21:48:20 +0200

    solves maximum difference between 2 elements

---
+++ src/MaximumDifferenceBetween2ElementsInArray.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+// https://www.techiedelight.com/find-maximum-difference-between-two-elements-array/
+public class MaximumDifferenceBetween2ElementsInArray {
+    public static void main(String[] args) {
+        int[] array = StdIn.takenputAndGetArray();
+        System.out.println(maximumDifference(array));
+    }
+
+    private static int maximumDifference(int[] array) {
+        int[] bestDifferences = getBestPossibleDifferences(array);
+        return max(bestDifferences);
+    }
+
+    private static int max(int[] array) {
+        int max = Integer.MIN_VALUE;
+        for (int element : array) {
+            if (element > max) {
+                max = element;
+            }
+        }
+        return max;
+    }
+
+    private static int[] getBestPossibleDifferences(int[] array) {
+        int[] result = new int[array.length];
+        result[result.length - 1] = 0;
+
+        for (int index = result.length - 2, max = array[array.length - 1]; index >= 0 ; index--) {
+            if (array[index] > max) {
+                max = array[index];
+                result[index] = 0;
+            } else {
+                result[index] = max - array[index];
+            }
+        }
+
+        return result;
+    }
+}
