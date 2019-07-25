5e816fff3a90c6c6830d8a0a47474dd99385cbbc
Anish Sachdeva <anish.sachdeva@cern.ch>
2019-07-25 16:48:54 +0200

    solves googles proble for minimum absolute difference

---
+++ src/MinimumAbsoluteDifferenceInServers_Google.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+
+
+public class MinimumAbsoluteDifferenceInServers_Google {
+  public static void main(String[] args) {
+    int length = StdIn.getIntInput();
+    int[] array = new int[length];
+    StdIn.getIntInput(array);
+    int result = minimumAbsoluteDifference(array);
+    System.out.println(result);
+  }
+
+  private static int minimumAbsoluteDifference(int[] array) {
+    int sum = Arrays.stream(array).sum();
+    boolean[][] canCreateSumUsingSubset = getSubsetSumsData(array, (sum + 1) / 2) ;
+
+//    Printer.print(canCreateSumUsingSubset);
+
+    for (int row = canCreateSumUsingSubset.length - 1, column = canCreateSumUsingSubset[0].length - 1 ; column >= 0 ; column--) {
+      if (canCreateSumUsingSubset[row][column]) {
+        return Math.abs(2 * column - sum ) ;
+      }
+    }
+
+    return -1;
+  }
+
+  private static boolean[][] getSubsetSumsData(int[] array, int sum) {
+    int rows = array.length, columns = sum + 1;
+    boolean[][] subsetSumExists = new boolean[array.length][sum + 1];
+
+    // first column
+    for (int row = 0, column = 0; row < rows ; row++) {
+      subsetSumExists[row][column] = true;
+    }
+
+    // first row
+    for (int row = 0, column = 1 ; column < columns ; column++) {
+      subsetSumExists[row][column] = column == array[row] ;
+    }
+
+    // rest of the table
+    for (int row = 1 ; row < rows ; row++) {
+      for (int column = 1 ; column < columns ; column++) {
+        subsetSumExists[row][column] =
+                subsetSumExists[row-1][column]
+                || array[row] == column
+                || (array[row] < column ? subsetSumExists[row-1][column - array[row]] : false) ;
+
+      }
+    }
+
+    return subsetSumExists;
+  }
+}
--- src/Printer.java
+++ src/Printer.java
@@ -1,8 +1,21 @@
-public class Printer {
-    public static void print(int[] array) {
+class Printer {
+    static void print(int[] array) {
         for (int element : array) {
             System.out.print(element + " , ");
         }
         System.out.println();
+    }
+
+    static void print(boolean[] array) {
+        for (boolean value :  array) {
+            System.out.print(value + " , ");
+        }
+        System.out.println();
+    }
+
+    static void print(boolean[][] matrix) {
+        for (boolean[] array : matrix) {
+            print(array);
+        }
     }
 }
