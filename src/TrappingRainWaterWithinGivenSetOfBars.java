41c5491a2458ab3377d32f0c15771efba77dd300
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2019-07-26 19:59:32 +0200

    solves trapping rain water in bars

---
+++ src/TrappingRainWaterWithinGivenSetOfBars.java
@@ -0,0 +1,22 @@
+// https://www.techiedelight.com/trapping-rain-water-within-given-set-bars/
+public class TrappingRainWaterWithinGivenSetOfBars {
+    public static void main(String[] args) {
+        int[] bars = StdIn.takeInputAndGetArray();
+        System.out.println(maxTrappedWater(bars));
+    }
+
+    private static int maxTrappedWater(int[] array) {
+        int water = 0;
+        for (int index = array.length - 2, max = array[array.length - 1], proxy = 0 ; index >= 0 ; index--) {
+            if (max > array[index]) {
+                proxy += max - array[index];
+            } else {
+                water += proxy;
+                proxy = 0;
+                max = array[index];
+            }
+        }
+
+        return water;
+    }
+}
