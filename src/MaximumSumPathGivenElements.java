d3dda0fcc00c5da4e76ee6655ea2c99f356d70c0
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2019-07-26 19:39:58 +0200

    solves maximum sub path of elements of array
    Signed-off-by: anishLearnsToCode <anish_bt2k16@dtu.ac.in>

---
+++ src/MaximumSumPathGivenElements.java
@@ -0,0 +1,46 @@
+// https://www.techiedelight.com/find-maximum-sum-path-involving-elements-given-arrays/
+public class MaximumSumPathGivenElements {
+    public static void main(String[] args) {
+        int[] first = StdIn.takeInputAndGetArray();
+        int[] second = StdIn.takeInputAndGetArray();
+        System.out.println(maximumCommonSumPath(first, second));
+    }
+
+    private static int maximumCommonSumPath(int[] first, int[] second) {
+        int sum = 0;
+        for (int firstIndex = 0, secondIndex = 0, currentSumFirst = 0, currentSumSecond = 0 ;
+             firstIndex <= first.length && secondIndex <= second.length ; ) {
+
+            if (firstIndex == first.length) {
+                while (secondIndex < second.length) {
+                    currentSumSecond += second[secondIndex++];
+                }
+                sum += Math.max(currentSumFirst, currentSumSecond);
+                break;
+            }
+
+            if (secondIndex == second.length) {
+                while (firstIndex < first.length) {
+                    currentSumFirst += first[firstIndex++];
+                }
+                sum += Math.max(currentSumFirst, currentSumSecond);
+                break;
+            }
+
+
+            if (first[firstIndex] < second[secondIndex]) {
+                currentSumFirst += first[firstIndex++];
+            } else if (first[firstIndex] > second[secondIndex]){
+                currentSumSecond += second[secondIndex++];
+            } else {
+                sum += Math.max(currentSumFirst, currentSumSecond) + first[firstIndex];
+                currentSumFirst = 0;
+                currentSumSecond = 0;
+                firstIndex++;
+                secondIndex++;
+            }
+        }
+
+        return sum;
+    }
+}
