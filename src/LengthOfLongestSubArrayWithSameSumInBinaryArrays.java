718cd3de28b01d9a8816ca590e8115d6ef5d28e6
Anish Sachdeva <anish.sachdeva@cern.ch>
2019-08-07 18:51:49 +0200

    solves LengthOfLongestSubArrayWithSameSumInBinaryArrays

---
+++ src/LengthOfLongestSubArrayWithSameSumInBinaryArrays.java
@@ -0,0 +1,48 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class LengthOfLongestSubArrayWithSameSumInBinaryArrays {
+  public static void main(String[] args) {
+    int[] first = StdIn.takeInputAndGetIntArray();
+    int[] second = StdIn.takeInputAndGetIntArray();
+    System.out.println(lengthOfLongestSubArrayWithSameSum(first, second));
+  }
+
+  private static int lengthOfLongestSubArrayWithSameSum(int[] first, int[] second) {
+    int[] firstArraySum = subArraySums(first);
+    int[] secondArraySum = subArraySums(second);
+    int[] difference = difference(firstArraySum, secondArraySum);
+    Map<Integer, Integer> differenceIndices = new HashMap<>();
+    differenceIndices.put(0, -1);
+
+    int max = 0;
+    for (int index = 0 ; index < difference.length ; index++) {
+      if (differenceIndices.containsKey(difference[index])) {
+        max = Math.max(max, index - differenceIndices.get(difference[index]));
+      } else {
+        differenceIndices.put(difference[index], index);
+      }
+    }
+
+    return max;
+  }
+
+  private static int[] subArraySums(int[] array) {
+    int[] result = new int[array.length];
+    result[0] = array[0];
+    for (int index = 1 ; index < array.length ; index++) {
+      result[index] = result[index - 1] + array[index];
+    }
+
+    return result;
+  }
+
+  private static int[] difference(int[] first, int[] second) {
+    int[] result = new int[first.length];
+    for (int index = 0 ; index < first.length ; index++) {
+      result[index] = first[index] - second[index];
+    }
+    return result;
+  }
+}
